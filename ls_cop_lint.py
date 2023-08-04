043c2e1fdb8514a6f27da202b8e01a07ca2d6799
Ganesh B. Nalawade <ganesh634@gmail.com>
2023-08-04 18:12:43 +0530

    Run ansible-lint using API

---
+++ ls_cop_lint.py
@@ -0,0 +1,75 @@
+import os
+import tempfile
+import yaml
+
+from copy import deepcopy
+from ansiblelint.rules import RulesCollection
+from ansiblelint.runner import Runner
+from ansiblelint.constants import DEFAULT_RULESDIR
+from ansiblelint.config import options as default_options
+from ansiblelint.runner import LintResult, _get_matches
+from ansiblelint.file_utils import Lintable
+from ansiblelint.config import Options
+from ansiblelint.transformer import Transformer
+import timeit
+
+completion = """---
+- hosts: localhost
+  tasks:
+  - name: disable ufw service
+    ansible.builtin.service:
+      name: ufw
+      enabled: false
+      state: stopped
+    when: '"ufw" in services'
+"""
+
+
+def run_linter(
+    config_options: Options,
+    default_rules_collection: RulesCollection,
+    inline_completion: str,
+) -> str:
+    """Runs the Runner to populate a LintResult for a given snippet."""
+    # create a temporary file
+    transformed_completion = inline_completion
+
+    with tempfile.NamedTemporaryFile(
+        mode="w", delete=False, suffix=".yml"
+    ) as temp_file:
+        # write the YAML string to the file
+        temp_file.write(inline_completion)
+        # get the path to the file
+        temp_completion_path = temp_file.name
+
+    config_options.lintables = [temp_completion_path]
+    result = _get_matches(rules=default_rules_collection, options=config_options)
+    # lintable = Lintable(temp_completion_path, kind="playbook")
+    # result = Runner(lintable, rules=default_rules_collection).run()
+    run_transform(result, config_options)
+
+    # read the transformed file
+    with open(temp_completion_path, "r", encoding="utf-8") as yaml_file:
+        transformed_completion = yaml.safe_load(yaml_file)
+
+    # delete the temporary file
+    os.remove(temp_completion_path)
+
+    return transformed_completion
+
+
+def run_transform(lint_result: LintResult, config_options: Options):
+    transformer = Transformer(result=lint_result, options=config_options)
+    transformer.run()
+
+
+new_options = deepcopy(default_options)
+new_options.write_list = ["all"]
+transformed_completion = run_linter(
+    new_options, RulesCollection(rulesdirs=[DEFAULT_RULESDIR]), completion
+)
+print(transformed_completion)
+# rules = RulesCollection(rulesdirs=[DEFAULT_RULESDIR], options=new_options)
+# rules.register()
+# results = Runner(test_suggestion, rules=rules).run()
+# print(results)
